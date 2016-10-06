6f78a70cca2798ac39615b38b056f4a561182fe5
PierreBanwarth <pi.banwarth@gmail.com>
2016-10-06 09:44:33 +0200

    Addding a new services and assiociated tests, now we can see how many cars are free and how many free space for each station

--- sources/modules/screens/home/home.controller.js
+++ sources/modules/screens/home/home.controller.js
@@ -38,25 +38,39 @@
 
       logger.log('init stations');
       stationService
+          .getDataStation()
+          .then( function( dataStation){
+            vm.dataStation = dataStation;
+            console.log(vm.dataStation);
+      });
+
+      stationService
         .getAllStation()
         .then(function(allStation) {
           vm.allStation = allStation;
           vm.center = {
-            lat: vm.allStation.zone.areas[0].area_map_lat,
-            lng: vm.allStation.zone.areas[0].area_map_lng,
+            lat:  vm.allStation.zone.areas[0].area_map_lat,
+            lng:  vm.allStation.zone.areas[0].area_map_lng,
             zoom: vm.allStation.zone.areas[0].map_level
           };
           var stations = vm.allStation.zone.areas[0].stations;
           vm.markers = [];
           for ( var i = 0; i < stations.length; i++){
-            var marker = {
+            var id = stations[i].station_id;
+            if ( id){
+              var availableCar =  vm.dataStation[id].station.available_car;
+              var totalCar =  vm.dataStation[id].station.parking_space_free;
+              var marker = {
               lat: stations[i].station_lat,
               lng: stations[i].station_lng,
               focus: false,
-              message: stations[i].station_name,
-              draggable: false
+              message: stations[i].station_name + ' ' + availableCar + '/' + totalCar,
+              draggable: false,
+              availableCar: availableCar,
+              totalSpace: totalCar
             };
             vm.markers.push(marker);
+            }
           }
         })
         .finally(function() {
@@ -64,7 +78,5 @@
         });
     }
     init();
-
   }
-
 })();
--- sources/modules/web-services/stations/station.service.js
+++ sources/modules/web-services/stations/station.service.js
@@ -1,24 +1,27 @@
 (function() {
-  'use strict';
-  angular
-    .module('app')
-    .factory('stationService', stationService);
-  /**
-   * station service: allows to get station of the day.
-   */
-  function stationService($q,
+	'use strict';
+	angular
+	.module('app')
+	.factory('stationService', stationService);
+	/**
+	* station service: allows to get station of the day.
+	*/
+	function stationService($q,
 						contextService,
+						logger,
 						restService) {
-	/*
-    * Constants
-    */
-    var ROUTES = {
-		allStation: ''
-    };
-	/*
-    * Public interface
-    */
+						/*
+						* Constants
+						*/
+						var ROUTES = {
+							allStation: 'http://www.metromobilite.fr/data/Carto/Statique/ListeDesStationsOMMS.json',
+							dataStation: 'http://www.metromobilite.fr/data/Carto/Dynamique/dynDetailDesVehiculesOMMS.json'
+						};
+						/*
+    					* Public interface
+    					*/
     var service = {};
+    logger = logger.getLogger('stationService');
      /**
      * Get all station items
      */
@@ -27,16 +30,29 @@
         .get(ROUTES.allStation, null, true)
         .then(function(response) {
           if (response.data) {
-            console.log(response);
             return response.data;
-
           }
           return $q.reject();
         })
         .catch(function() {
-          return 'Error, could not load stations';
+           logger.log('Error, could not load stations');
+        });
+    };
+    service.getDataStation = function() {
+		return restService
+        .get(ROUTES.dataStation, null, true)
+        .then(function(response) {
+			if (response.data) {
+				var result = [];
+				result =  angular.fromJson(response.data);
+				return result;
+			}
+			return $q.reject();
+		})
+        .catch(function() {
+           logger.log('Error, could not load stations');
         });
     };
     return service;
-  }
+}
 })();
